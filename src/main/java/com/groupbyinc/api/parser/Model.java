275f30191cd2117aad1b617dc9d95e5568e1b803
Osman Currim <osman.currim@groupbyinc.com>
2015-05-20 16:06:01 -0400

    remove old source files

--- src/main/java/com/groupbyinc/api/parser/Model.java
+++
@@ -1,9 +0,0 @@
-package com.groupbyinc.api.parser;
-
-import java.io.Serializable;
-
-public class Model implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-}
--- src/main/java/com/groupbyinc/injector/StaticInjector.java
+++
@@ -1,7 +0,0 @@
-package com.groupbyinc.injector;
-
-public interface StaticInjector<T> {
-    public T get();
-
-    public void set(T pInjectedObject);
-}
