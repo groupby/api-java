46ae848e9d948dd93e415e0d7f0a0c4b2e7ced05
Osman Currim <osman.currim@groupbyinc.com>
2015-09-02 16:48:15 -0400

    update debug info

--- common/src/main/java/com/groupbyinc/api/model/DebugInfo.java
+++ common/src/main/java/com/groupbyinc/api/model/DebugInfo.java
@@ -10,16 +10,22 @@ import java.util.Map;
  */
 public class DebugInfo {
     @JsonProperty
+    Map<String, Object> rawRequest;
+    @JsonProperty
     Map<String, Object> rawResponse;
     @JsonProperty
-    Map<String, Object> rawRequest;
+    Map<String, Object> rawAggregationsRequest;
+    @JsonProperty
+    Map<String, Object> rawAggregationsResponse;
 
-    public static DebugInfo createDebugInfo(Map<String, Object> rawRequest, Map<String, Object> rawResponse) {
+    public static DebugInfo createDebugInfo(Map<String, Object> rawRequest, Map<String, Object> rawResponse,
+                                            Map<String, Object> rawAggregationsRequest,
+                                            Map<String, Object> rawAggregationsResponse) {
         DebugInfo debugInfo = new DebugInfo();
         debugInfo.rawRequest = rawRequest;
         debugInfo.rawResponse = rawResponse;
+        debugInfo.rawAggregationsRequest = rawAggregationsRequest;
+        debugInfo.rawAggregationsResponse = rawAggregationsResponse;
         return debugInfo;
     }
-
-
 }
