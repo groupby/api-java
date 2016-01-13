cd3ab94c816afe63e8b4b6e1739e48eea17ab960
Osman Currim <osman.currim@groupbyinc.com>
2016-01-13 01:25:29 -0500

    add support for bring to top

--- src/main/java/com/groupbyinc/api/model/Biasing.java
+++ src/main/java/com/groupbyinc/api/model/Biasing.java
@@ -1,10 +1,40 @@
 package com.groupbyinc.api.model;
 
+import com.groupbyinc.common.jackson.annotation.JsonInclude;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <code>
- + A biasing profile defined at query time, possible settings include:
+ * A biasing profile defined at query time.
  + </code>
  * Created by groupby on 1/12/16.
  */
 public class Biasing {
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<String> bringToTop = new ArrayList<String>();
+
+    /**
+     * @return The list of product IDs
+     */
+    public List<String> getBringToTop() {
+        return bringToTop;
+    }
+
+    /**
+     * <code>
+     * A list of product IDs to bring to the top of the result set. This list
+     * will ensure that the products are included in the result set and appear in the order
+     * defined.
+     * </code>
+     *
+     * @param bringToTop
+     *      The list of productIds.
+     * @return
+     */
+    public Biasing setBringToTop(List<String> bringToTop) {
+        this.bringToTop = bringToTop;
+        return this;
+    }
 }
--- src/main/java/com/groupbyinc/api/request/Biasing.java
+++ src/main/java/com/groupbyinc/api/request/Biasing.java
@@ -1,8 +1,24 @@
 package com.groupbyinc.api.request;
 
+import com.groupbyinc.common.jackson.annotation.JsonInclude;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * Created by groupby on 1/12/16.
  */
 public class Biasing {
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<String> bringToTop = new ArrayList<String>();
+
+    public List<String> getBringToTop() {
+        return bringToTop;
+    }
+
+    public Biasing setBringToTop(List<String> bringToTop) {
+        this.bringToTop = bringToTop;
+        return this;
+    }
 }
