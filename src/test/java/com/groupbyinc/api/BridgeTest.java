ec954cb83969ea8ce43e51975524604def4b5804
Will Warren <will.warren@groupbyinc.com>
2016-05-18 13:51:24 -0400

    Added headers and skip cache for semantic layer

--- src/test/java/com/groupbyinc/api/BridgeTest.java
+++ src/test/java/com/groupbyinc/api/BridgeTest.java
@@ -3,17 +3,27 @@ package com.groupbyinc.api;
 import com.groupbyinc.common.util.ResourceUtils;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-/**
- * Created by groupby on 10/23/15.
- */
 public class BridgeTest {
 
   @Test
   public void testGB1546() throws Exception {
     assertResponse("/GB-1546.json", false);
     assertResponse("/GB-1546.cbor", true);
+  }
+
+  @Test
+  public void testCaching(){
+    CloudBridge test = new CloudBridge("aoeu", "aoeu");
+    assertEquals(0, test.getHeaders().size());
+    test.setCachingEnabled(false);
+    assertEquals("Skip-Caching", test.getHeaders().get(0).getName());
+    test.setCachingEnabled(false);
+    assertEquals(1, test.getHeaders().size());
+    test.setCachingEnabled(true);
+    assertEquals(0, test.getHeaders().size());
   }
 
   private void assertResponse(String resourcePath, boolean binary) throws Exception {
