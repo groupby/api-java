0d62f07b57aabe410d97bab16477368bfa008cc4
Osman Currim <osman.currim@groupbyinc.com>
2016-11-04 20:56:45 -0400

    Revert "update DebugInfo"
    This reverts commit 76b953825f7c95ce9020bff14ffbc9fb784bc26b.

--- src/main/java/com/groupbyinc/api/model/DebugInfo.java
+++ src/main/java/com/groupbyinc/api/model/DebugInfo.java
@@ -12,12 +12,16 @@ public class DebugInfo {
 
   @JsonProperty protected Map<String, Object> rawRequest;
   @JsonProperty protected Map<String, Object> rawResponse;
+  @JsonProperty protected Map<String, Object> rawAggregationsRequest;
+  @JsonProperty protected Map<String, Object> rawAggregationsResponse;
 
   public static DebugInfo createDebugInfo(
-      Map<String, Object> rawRequest, Map<String, Object> rawResponse) {
+      Map<String, Object> rawRequest, Map<String, Object> rawResponse, Map<String, Object> rawAggregationsRequest, Map<String, Object> rawAggregationsResponse) {
     DebugInfo debugInfo = new DebugInfo();
     debugInfo.rawRequest = rawRequest;
     debugInfo.rawResponse = rawResponse;
+    debugInfo.rawAggregationsRequest = rawAggregationsRequest;
+    debugInfo.rawAggregationsResponse = rawAggregationsResponse;
     return debugInfo;
   }
 }
