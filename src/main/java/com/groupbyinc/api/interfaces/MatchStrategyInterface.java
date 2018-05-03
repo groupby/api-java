31b3870110bbd99da012c71cf41d61e54a5b4754
Osman Currim <osman.currim@groupbyinc.com>
2018-05-03 14:02:19 -0400

    cleanup

--- src/main/java/com/groupbyinc/api/interfaces/MatchStrategyInterface.java
+++ src/main/java/com/groupbyinc/api/interfaces/MatchStrategyInterface.java
@@ -2,11 +2,6 @@ package com.groupbyinc.api.interfaces;
 
 import java.util.List;
 
-/**
- * MatchStrategy
- *
- * @author Ben Teichman
- */
 public interface MatchStrategyInterface {
 
   List<? extends PartialMatchRuleInterface> getRules();
