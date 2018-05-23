8365c89d7c1014bf94848e6ebedd6cb74938a2d8
Yash Bhamare <Yash.Bhamare@groupbyinc.com>
2018-05-23 16:06:34 -0400

    f

--- src/main/java/com/groupbyinc/api/model/ResultsMetadata.java
+++ src/main/java/com/groupbyinc/api/model/ResultsMetadata.java
@@ -43,7 +43,7 @@ public class ResultsMetadata {
 
   public ResultsMetadata setCached(boolean cached) {
     this.isCache = cached;
-    this.fromCache = !isCached();
+    this.fromCache = !this.isCache;
     return this;
   }
 }
